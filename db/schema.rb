a46b07582bccb00dcc09f97bf17951ca52a15ca7
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-10-21 19:21:49 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181021183350) do
+ActiveRecord::Schema.define(version: 20181021192004) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,18 @@ ActiveRecord::Schema.define(version: 20181021183350) do
   end
 
   add_index "neighborhoods", ["city_id"], name: "index_neighborhoods_on_city_id"
+
+  create_table "reservations", force: :cascade do |t|
+    t.date     "checkin"
+    t.date     "checkout"
+    t.integer  "listing_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reservations", ["listing_id"], name: "index_reservations_on_listing_id"
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
